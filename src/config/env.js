c61734d0985b43405edde8eb48768036e65b223e
astrodev131 <astro.dev131@gmail.com>
2024-12-11 11:32:31 +0900

    finish

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,4 @@
 require("dotenv").config();
-
-const JWT_SECRET = process.env.JWT_SECRET;
-
 module.exports = {
   PORT: process.env.PORT || 3000,
   DB_URI: process.env.DB_URI,
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validators/authValidator");
+const validateRequest = require("../middlewares/validationMiddleware");
 
 const router = express.Router();
 
-router.post("/register", registerUser); // Register a new user
-router.post("/login", loginUser); // Login user
+// Register route
+router.post("/register", registerValidation, validateRequest, registerUser);
+
+// Login route
+router.post("/login", loginValidation, validateRequest, loginUser);
 
 module.exports = router;
